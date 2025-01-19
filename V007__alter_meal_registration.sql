eed4e6b984c633182210239fdc8f2514a701cca2
thselukas <lukimayer12345@gmail.com>
2025-01-19 14:28:50 +0100

    altered

--- V007__alter_meal_registration.sql
+++ V007__alter_meal_registration.sql
@@ -1,9 +1,9 @@
 ALTER TABLE [dbo].[MealRegistrations]
     ADD [paymentMethod] VARCHAR(10) NOT NULL CHECK (paymentMethod IN ('cash', 'card')) DEFAULT 'cash',
         [paymentReceived] BIT NOT NULL DEFAULT 0,
-        [paymentReceivedAt] DATETIME NULL,
+        [paymentReceivedAt] DATETIME2 NULL,
         [sick] BIT NOT NULL DEFAULT 0,
-        [createdAt] DATETIME NOT NULL DEFAULT GETDATE(),
-        [pickedUpAt] DATETIME NULL;
+        [createdAt] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [pickedUpAt] DATETIME2 NULL;
 
 
